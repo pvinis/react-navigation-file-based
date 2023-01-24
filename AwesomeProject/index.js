f58c989d9fb06a506e38cfcb3db219c82d50af31
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-24 21:49:09 +0000

    trying

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -2,6 +2,7 @@
 // import App from './App';
 // import {name as appName} from './app.json';
 
+// import 'expo-router/entry';
 // AppRegistry.registerComponent(appName, () => App);
 
 import 'expo-router/entry';
