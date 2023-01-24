0bfd06e7fc68196ce4e1dac57121817f2269baca
Pavlos Vinieratos <pvinis@gmail.com>
2023-01-24 22:03:58 +0000

    still nop

--- AwesomeProject/metro.config.js
+++ AwesomeProject/metro.config.js
@@ -1,12 +1,6 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 module.exports = {
   transformer: {
+    unstable_allowRequireContext: true,
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
